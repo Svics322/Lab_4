aee04fd5edf026651ec83e8a14c0d478a24702d7
Sevicss <ahieofglory@gmail.com>
2024-09-23 03:44:28 +0300

    Lab 4 Task 1 UnitTest

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab4\LexPermutations.cpp"
+#include<vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestN3)
+		{
+			vector<vector<int>> expected = {
+				{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}
+			};
+			LexPermutations lexPerm("3");
+			vector<vector<int>> result = lexPerm.find();
+			Assert::AreEqual((int)expected.size(), (int)result.size(), L"Неправильна кількість перестановок для N=3");
+
+			for (size_t i = 0; i < expected.size(); ++i) {
+				for (size_t j = 0; j < expected[i].size(); ++j) {					
+					wstring message = L"Неправильна перестановка на позиції " + to_wstring(i) + L", елемент " + to_wstring(j);
+					Assert::AreEqual(expected[i][j], result[i][j], message.c_str());
+				}
+			}
+		}
+		TEST_METHOD(TestN3Error)
+		{
+			vector<vector<int>> expected = {
+				{1, 2, 3}, {1, 3, 2}, {2, 3, 1}, {3, 1, 2} // Помилка: відсутні перестановки {2, 1, 3} і {3, 2, 1}
+
+			};
+			LexPermutations lexPerm("3");
+			vector<vector<int>> result = lexPerm.find();
+			Assert::AreEqual((int)expected.size(), (int)result.size(), L"Неправильна кількість перестановок для N=3");
+
+			for (size_t i = 0; i < expected.size(); ++i) {
+				for (size_t j = 0; j < expected[i].size(); ++j) {
+					wstring message = L"Неправильна перестановка на позиції " + to_wstring(i) + L", елемент " + to_wstring(j);
+					Assert::AreEqual(expected[i][j], result[i][j], message.c_str());
+				}
+			}
+		}
+	};
+}
